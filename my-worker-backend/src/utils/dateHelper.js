d7199cf4749184dda28bba95c26725a1ecdacdcb
BigSky-1208 <sora.yamashita@neo-career.co.jp>
2026-07-29 15:50:14 +0900

    Update dateHelper.js

--- my-worker-backend/src/utils/dateHelper.js
+++ my-worker-backend/src/utils/dateHelper.js
@@ -1,1 +1,18 @@
+export function getJSTDate(date = new Date()) {
+  return new Date(date.getTime() + 9 * 60 * 60 * 1000);
+}
 
+export function getJSTISOString(date = new Date()) {
+  const jst = getJSTDate(date);
+  return jst.toISOString().replace('Z', '+09:00');
+}
+
+export function getJSTDateString(date = new Date()) {
+  const jst = getJSTDate(date);
+  return jst.toISOString().split('T')[0];
+}
+
+export function buildJSTDateTimeISO(dateStr, timeStr) {
+  const formattedTime = timeStr.padStart(5, '0');
+  return `${dateStr}T${formattedTime}:00+09:00`;
+}
