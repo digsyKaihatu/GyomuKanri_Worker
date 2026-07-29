d8d3ced2497741199bf56168a23c0847efa73a23
BigSky-1208 <sora.yamashita@neo-career.co.jp>
2026-07-29 15:50:57 +0900

    Update cacheService.js

--- my-worker-backend/src/services/cacheService.js
+++ my-worker-backend/src/services/cacheService.js
@@ -1,1 +1,12 @@
+import { CACHE_VERSION } from '../config/constants.js';
 
+export async function purgeDailySummaryCache(requestUrl, dateStr) {
+  try {
+    const cache = caches.default;
+    const url = new URL(requestUrl);
+    const purgeTargetUrl = `${url.origin}/get-daily-summary?date=${dateStr}&${CACHE_VERSION}`;
+    await cache.delete(new Request(purgeTargetUrl));
+  } catch (err) {
+    console.error(`Failed to purge CDN cache for ${dateStr}:`, err);
+  }
+}
