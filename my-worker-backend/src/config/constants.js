f01df96968ecd5679953fdd58c207fb11a990ce4
BigSky-1208 <sora.yamashita@neo-career.co.jp>
2026-07-29 15:49:57 +0900

    Update constants.js

--- my-worker-backend/src/config/constants.js
+++ my-worker-backend/src/config/constants.js
@@ -1,1 +1,9 @@
 
+export const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+};
+
+// CDNキャッシュキーのバージョン一元管理
+export const CACHE_VERSION = "v=20260729";
