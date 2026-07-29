2f83f4fe970ca2b1858b98d85883938c81cb07e0
BigSky-1208 <sora.yamashita@neo-career.co.jp>
2026-07-29 15:50:36 +0900

    Update cryptoHelper.js

--- my-worker-backend/src/utils/cryptoHelper.js
+++ my-worker-backend/src/utils/cryptoHelper.js
@@ -1,1 +1,63 @@
+function btoaUrl(str) {
+  return btoa(str).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
 
+function str2ab(pem) {
+  const pemHeader = "-----BEGIN PRIVATE KEY-----";
+  const pemFooter = "-----END PRIVATE KEY-----";
+  const pemContents = pem.substring(pem.indexOf(pemHeader) + pemHeader.length, pem.indexOf(pemFooter)).replace(/\s/g, '');
+  const binaryString = atob(pemContents);
+  const bytes = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
+export async function getAccessToken(serviceAccount) {
+  const pem = serviceAccount.private_key.replace(/\\n/g, '\n');
+  const clientEmail = serviceAccount.client_email;
+  const header = { alg: 'RS256', typ: 'JWT' };
+  const now = Math.floor(Date.now() / 1000);
+
+  const claim = {
+    iss: clientEmail,
+    scope: 'https://www.googleapis.com/auth/cloud-platform',
+    aud: 'https://oauth2.googleapis.com/token',
+    exp: now + 3600,
+    iat: now,
+  };
+
+  const encodedHeader = btoaUrl(JSON.stringify(header));
+  const encodedClaim = btoaUrl(JSON.stringify(claim));
+  const binaryKey = str2ab(pem);
+
+  const key = await crypto.subtle.importKey(
+    'pkcs8',
+    binaryKey,
+    { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
+    false,
+    ['sign']
+  );
+
+  const signature = await crypto.subtle.sign(
+    'RSASSA-PKCS1-v1_5',
+    key,
+    new TextEncoder().encode(`${encodedHeader}.${encodedClaim}`)
+  );
+
+  const jwt = `${encodedHeader}.${encodedClaim}.${btoaUrl(String.fromCharCode(...new Uint8Array(signature)))}`;
+
+  const tokenResp = await fetch('https://oauth2.googleapis.com/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: `grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=${jwt}`,
+  });
+
+  const data = await tokenResp.json();
+  if (!tokenResp.ok) {
+    throw new Error(`Google OAuth アクセストークンの取得に失敗しました: ${JSON.stringify(data)}`);
+  }
+
+  return data.access_token;
+}
